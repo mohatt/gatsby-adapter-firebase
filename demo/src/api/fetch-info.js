ecd8a77d587ad935c9fa6b23792b1476c172b3b8
Mohamed Elkholy <mkh117@gmail.com>
2025-10-14 09:27:46 +0400

    fix: remove test-fetch-info endpoint

--- demo/src/api/fetch-info.js
+++ demo/src/api/fetch-info.js
@@ -1,9 +1,4 @@
 export default function handler(req, res) {
-  console.log('req.url', req.url)
-  console.log('req.query', req.query)
-  console.log('req.cookies', req.cookies)
-  console.log('req.path', req.path)
-  console.log('req.query', req.query)
   res.status(200).json({ data: ['hello', `world`] })
 }
 
